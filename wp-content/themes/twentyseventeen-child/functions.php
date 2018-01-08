1a77811ea284190ffc2280cfb65ee59973223e86
jalmiShreya <shreya@ajency.in>
2018-01-08 16:54:02 +0530

    pledge updated

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,19 +1,71 @@
 <?php
-function my_theme_enqueue_styles() {
+//* Code goes here
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+add_action('the_champ_login_user','wpse38285_wp_footer');
+function addPledge()
+{
+
+  /*wp_register_script('my_amazing_script', get_template_directory_uri() . '/removeArrows.js', array('jquery'),'1.1', true);
+    wp_enqueue_script('my_amazing_script');
+	*/
 
 
+update_user_meta(1,'test',5465487);
+
+   $js = <<<JS
+   <script type="text/javascript">
+       jQuery('<div />')
+           .html('<p>s <strong>$current_user->user_login</strong><br /><small>(click to close)</small></p>')
+           .css({
+               'width': '300px',
+               'position': 'absolute',
+               'left': '50%',
+               'marginLeft': '-160px',
+               'top': '100px',
+               'backgroundColor': '#cdcdcd',
+               'textAlign': 'center',
+               'padding': '10px'
+           })
+           .appendTo('body')
+           .on('click', function() { jQuery(this).remove(); } );
+           alert('dsds')
+   </script>
+JS;
+   echo $js;
+
+	
+}
+
+function wpse38285_wp_footer() {
 
 
+   $js = <<<JS
+   <script type="text/javascript">
+       jQuery('<div />')
+           .html('<p>s <strong>$current_user->user_login</strong><br /><small>(click to close)</small></p>')
+           .css({
+               'width': '300px',
+               'position': 'absolute',
+               'left': '50%',
+               'marginLeft': '-160px',
+               'top': '100px',
+               'backgroundColor': '#cdcdcd',
+               'textAlign': 'center',
+               'padding': '10px'
+           })
+           .appendTo('body')
+           .on('click', function() { jQuery(this).remove(); } );
+           alert('dsds')
+   </script>;
 
-?>
+JS;
+   echo $js;
+}
+//add_action( 'wp_footer', 'wpse38285_wp_footer' );
+
+	
