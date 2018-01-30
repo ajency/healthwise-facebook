c60b52d34b91afd953e4f6ace0330bcf362de94c
Nutan Kamat <nutan@ajency.in>
2018-01-30 14:20:00 +0530

    themes and plugins added

---
+++ wp-content/themes/simplemag-child/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * SimpleMag child theme functions & definitions
+**/
+
+
+/**
+ * Parent theme style.css
+**/
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+?>
